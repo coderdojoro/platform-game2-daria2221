8cad1c04b849d9794dd192d47734d326d7164f03
daria <daria.ivan5b@gmail.com>
2020-11-15 22:03:33 +0200

    Daria

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,35 +1,35 @@
 /// <reference path="../../typings/phaser.d.ts" />
 // @ts-check
 
-import Phaser from 'Phaser';
+import Phaser from 'phaser';
 
 class Game extends Phaser.Scene {
   preload() {
     this.load.image('mage', 'assets/mage/mage.png');
-    this.load.spritesheet('idle-spreadsheet', 'assets/mage/idle.png', { frameWidth: 128, frameHight: 128 });
+    this.load.spritesheet('idle-spritesheet', 'assets/mage/idle.png', { frameWidth: 128, frameHeight: 128 });
+    create() {
+
+
+      this.anims.create({
+
+        key: 'hero-idle',
+        frames: [
+          { frame: 0, key: 'mage', duration: 5000 },
+          ...this.anims.generateFrameNumbers('idle-spritesheet', {})
+        ],
+        frameRate: 6,
+        repeat: -1
+      })
+
+
+
+      let hero = this.physics.add.sprite(400, 300, 'mage');
+
+      hero.body.setCollideWorldBounds(true);
+      hero.body.setSize(33, 54);
+      hero.body.setOffset(27, 57);
+      hero.anims.play('hero-idle');
+    }
   }
-  create() {
 
-
-    this.anims.create({
-
-      key: 'hero-idle',
-      frames: [
-        { frame: 0, key: 'mage', duration: 5000 },
-        ...this.anims.generateFrameNumbers('idle-spritesheet', {})
-      ],
-      frameRate: 6,
-      repeat: -1
-    })
-
-
-
-    let hero = this.physics.add.sprite(400, 300, 'mage');
-
-    hero.body.setCollideWorldBounds(true);
-    hero.body.setSize(33, 54);
-    hero.body.setOffset(27, 57);
-  }
-}
-
-export default Game; 
+  export default Game; 
